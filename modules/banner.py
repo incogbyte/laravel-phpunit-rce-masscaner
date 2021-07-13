43e97fa9f91f5c465db8f2de001cd966db0da650
Rodolfo Tavares <71647887+rodnt@users.noreply.github.com>
2021-07-13 18:16:00 -0300

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -2,7 +2,7 @@ class Banner():
     def b():
         return (f"""
 
-        By: r00td3v / https://r00td3v.github.io / TW: @r00td3v
+        By: r00td3v / https://rodnt.github.io / TW: @rodntt
 
 
  ██▓    ▄▄▄       ██▀███   ▄▄▄    ██▒   █▓▓█████  ██▓        ██▓███   ██░ ██  ██▓███   █    ██  ███▄    █  ██▓▄▄▄█████▓    ██▀███   ▄████▄  ▓█████ 
